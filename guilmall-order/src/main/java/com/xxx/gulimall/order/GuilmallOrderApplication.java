370042e4a168f94ee6988061104eed10c28b4f08
xiexiaoxuan <xiexiaoxuan1998@qq.com>
2022-08-15 15:12:17 +0800

    项目初始结构提交

---
+++ guilmall-order/src/main/java/com/xxx/gulimall/order/GuilmallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.xxx.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilmallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuilmallOrderApplication.class, args);
+    }
+
+}
